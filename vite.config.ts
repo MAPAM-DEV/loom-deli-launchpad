202eb58f26bb1e09ab06f8611597079637309db1
MAPAM-DEV <mapam.ucty@gmail.com>
2026-02-02 16:21:05 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  // base: '/loom-deli-launchpad/',
+  base: '/loom-deli-launchpad/',
   server: {
     host: "::",
     port: 8080,
